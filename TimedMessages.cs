609f110c451ec36920ca67209bb4646057729ab2
royboot <rgrizell467@gmail.com>
2025-07-21 23:59:51 +0200

    name change+ bug hunting

--- TimedMessages.cs
+++ TimedMessages.cs
@@ -1,25 +1,44 @@
 ﻿using System.Collections.Generic;
+using Oxide.Core.Libraries.Covalence;
+
 namespace Oxide.Plugins
 {
     [Info("TimedMessages", "Royboot", "0.1.0")]
     [Description("Makes epic stuff happen")]
-    class MessagePlugin : CovalencePlugin
+    class TimedMessages : CovalencePlugin
     {
         private static PluginConfig? _config;
         private List<float> _intervals;// 3600 seconds = 1 hour
         private List<string> _colors;
         private List<string> _messages;
+        private static bool _isTimerRunning = false;
         private void Init()
         {
+            _isTimerRunning = false;
             LoadConfig();
             _intervals = _config.Intervals;
             _colors = _config.Colors;
             _messages = _config.Messages;
             timer.Every(_intervals[0], () =>
             {
+                _isTimerRunning = true;
                 Puts("Message plugin enabled.");
                 BroadcastWipeMessage(_messages,_colors);
             });
+        }
+
+        void Loaded()
+        {
+            _isTimerRunning = false;
+            if (!_isTimerRunning)
+            {
+                timer.Every(_intervals[0], () =>
+                {
+                    Puts("Timed Messages plugin enabled.");
+                    BroadcastWipeMessage(_messages, _colors);
+                });
+
+            }
         }
         private void BroadcastWipeMessage(List<string> messages,List<string> colors)
         {
@@ -69,22 +88,55 @@ namespace Oxide.Plugins
                 },
                 Colors = new List<string>()
                 {
-                    "<color=#ffa500>",
-                    "<color=#00ffff>",
+                    "#ffa500",
+                    "#00ffff",
                 },
                 Intervals = new List<float>()
                 {
-                    3600f,
+                    60.0f,
                 }
             };
+        }
+        [Command("Broadcast")]
+        private void BroadCast(IPlayer sender, string command, string[] args)
+        {
+            var playerlist = BasePlayer.activePlayerList;
+            string message = string.Join(" ", args);
+            if (args.Length == 0)
+            {
+                sender.Message("Usage: /broadcast <message>");
+                return;
+            }
+            foreach (var player in playerlist)
+            {
+                if (player != null)
+                {
+                    if (args.Length > 0 && args.Length == 1)
+                    {
+                        player.ChatMessage(message);
+                    }
+                    sender.Message($"Message send succesful {message}");
+                }
+            }
+            if (!_isTimerRunning)
+            {
+                Puts(_intervals[0].ToString());
+                timer.Every(_intervals[0], () =>
+                {
+                    Puts("Timer started");
+                    BroadcastWipeMessage(_messages, _colors);
+                });
+            }
+
+            return;
         }
     }
     
 
     class PluginConfig
     {
-        public List<string> Messages { get; set; }
-        public List<string> Colors = new List<string>();
-        public List<float> Intervals;
+        public List<string> Messages { get; set; } = new List<string>();
+        public List<string> Colors { get; set; } = new List<string>();
+        public List<float> Intervals { get; set; } = new List<float>();
     }
 }
